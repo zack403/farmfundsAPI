4dfd8f72cd05b68bdce75fb4aaf6f7fd2dd39b79
Zack <aminuzack7@gmail.com>
2020-11-18 17:23:01 +0100

    fix error on excel upload middleware and error middleware

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,6 +1,6 @@
 module.exports = (err, req, res, next) => {
     res.status(500).send({
         status : 500,
-        error: err.message       
+        error: err.message === 'Validation error' ? err.errors[0].message : err.message      
       });
 }
